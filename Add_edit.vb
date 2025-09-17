a8c45f5e7048385d56d64c167c8dfc02f5705881
lol <125685532+Ren-Desuwa@users.noreply.github.com>
2025-09-18 00:56:50 +0800

    tangina mo jess

--- Add_edit.vb
+++ Add_edit.vb
@@ -1,17 +1,17 @@
 ﻿Imports System.Data.OleDb
 
 Public Class Add_edit
-    'Dim con As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\path\yourdb.accdb;")
+    Dim con As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:/;")
 
     Private Sub Add_edit_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'Try
-        '    con.Open()
-        '    MessageBox.Show("Connected to Access!")
-        'Catch ex As Exception
-        '    MessageBox.Show("Error: " & ex.Message)
-        'Finally
-        '    con.Close()
-        'End Try
+        Try
+            con.Open()
+            MessageBox.Show("Connected to Access!")
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            con.Close()
+        End Try
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
