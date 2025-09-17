6eb12cd01f818e1695687d5c10792d93482b42c6
lol <125685532+Ren-Desuwa@users.noreply.github.com>
2025-09-18 02:58:18 +0800

    cancel

--- Forms/Add_edit.vb
+++ Forms/Add_edit.vb
@@ -13,7 +13,18 @@ Public Class Add_edit
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-
+        'yung pang clear button'
+        Txt_InputLastName.Text = ""
+        Txt_InputFirstName.Text = ""
+        Txt_InputMiddleName.Text = ""
+        CheckBox_MiddleName.Checked = False
+        ComboBox_CivilStatus.SelectedIndex = -1
+        ComboBox_Suffix.SelectedIndex = -1
+        ComboBox_Gender.SelectedIndex = -1
+        DateTimePicker.Value = DateTime.Now
+        Txt_InputNationality.Text = ""
+        Txt_InputContact.Text = ""
+        Txt_InputAddress.Text = ""
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
