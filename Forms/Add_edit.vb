2dbed893986c656a1f0712341d2a99017ad66a44
Shan2411 <shandavid2411@gmail.com>
2025-09-18 02:37:36 +0800

    Clear Button tas yung dropdown list

--- Forms/Add_edit.vb
+++ Forms/Add_edit.vb
@@ -5,6 +5,21 @@ Public Class Add_edit
 
 
     Private Sub Add_edit_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Populate comboboxes'
+        ComboBox_Suffix.Items.Add("Jr.")
+        ComboBox_Suffix.Items.Add("Sr.")
+        ComboBox_Suffix.Items.Add("III")
+
+        ComboBox_Gender.Items.Add("Male")
+        ComboBox_Gender.Items.Add("Female")
+
+        ComboBox_CivilStatus.Items.Add("Single")
+        ComboBox_CivilStatus.Items.Add("Married")
+
+        'Make the Dropdownlist not be able to take in texts'
+        ComboBox_Suffix.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox_Gender.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox_CivilStatus.DropDownStyle = ComboBoxStyle.DropDownList
 
     End Sub
 
@@ -13,7 +28,18 @@ Public Class Add_edit
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-
+        'yung pang clear button'
+        Txt_InputLastName.Text = ""
+        Txt_InputFirstName.Text = ""
+        Txt_InputMiddleName.Text = ""
+        CheckBox_MiddleName.Checked = False
+        ComboBox_CivilStatus.SelectedIndex = -1
+        ComboBox_Suffix.SelectedIndex = -1
+        ComboBox_Gender.SelectedIndex = -1
+        DateTimePicker.Value = DateTime.Now
+        Txt_InputNationality.Text = ""
+        Txt_InputContact.Text = ""
+        Txt_InputAddress.Text = ""
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
