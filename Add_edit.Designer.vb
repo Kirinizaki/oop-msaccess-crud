2a9d9e3fb605fd6da53d6a776b4b542feb6731e3
Kirinizaki <jamesalba718@gmail.com>
2025-09-18 00:43:07 +0800

    ff

--- Add_edit.Designer.vb
+++ Add_edit.Designer.vb
@@ -43,39 +43,42 @@ Partial Class Add_edit
         Lbl_Contact = New Label()
         Lbl_Address = New Label()
         Txt_InputAddress = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
         SuspendLayout()
         ' 
         ' Txt_InputLastName
         ' 
-        Txt_InputLastName.Location = New Point(117, 25)
-        Txt_InputLastName.Margin = New Padding(2, 2, 2, 2)
+        Txt_InputLastName.Location = New Point(146, 31)
+        Txt_InputLastName.Margin = New Padding(2)
         Txt_InputLastName.Name = "Txt_InputLastName"
-        Txt_InputLastName.Size = New Size(255, 27)
+        Txt_InputLastName.Size = New Size(318, 31)
         Txt_InputLastName.TabIndex = 0
         ' 
         ' Txt_InputFirstName
         ' 
-        Txt_InputFirstName.Location = New Point(117, 63)
-        Txt_InputFirstName.Margin = New Padding(2, 2, 2, 2)
+        Txt_InputFirstName.Location = New Point(146, 79)
+        Txt_InputFirstName.Margin = New Padding(2)
         Txt_InputFirstName.Name = "Txt_InputFirstName"
-        Txt_InputFirstName.Size = New Size(255, 27)
+        Txt_InputFirstName.Size = New Size(318, 31)
         Txt_InputFirstName.TabIndex = 1
         ' 
         ' Txt_InputMiddleName
         ' 
-        Txt_InputMiddleName.Location = New Point(117, 102)
-        Txt_InputMiddleName.Margin = New Padding(2, 2, 2, 2)
+        Txt_InputMiddleName.Location = New Point(146, 128)
+        Txt_InputMiddleName.Margin = New Padding(2)
         Txt_InputMiddleName.Name = "Txt_InputMiddleName"
-        Txt_InputMiddleName.Size = New Size(255, 27)
+        Txt_InputMiddleName.Size = New Size(318, 31)
         Txt_InputMiddleName.TabIndex = 2
         ' 
         ' CheckBox_MiddleName
         ' 
         CheckBox_MiddleName.AutoSize = True
-        CheckBox_MiddleName.Location = New Point(117, 131)
-        CheckBox_MiddleName.Margin = New Padding(2, 2, 2, 2)
+        CheckBox_MiddleName.Location = New Point(146, 164)
+        CheckBox_MiddleName.Margin = New Padding(2)
         CheckBox_MiddleName.Name = "CheckBox_MiddleName"
-        CheckBox_MiddleName.Size = New Size(146, 24)
+        CheckBox_MiddleName.Size = New Size(174, 29)
         CheckBox_MiddleName.TabIndex = 3
         CheckBox_MiddleName.Text = "No Middle Name"
         CheckBox_MiddleName.UseVisualStyleBackColor = True
@@ -83,167 +86,197 @@ Partial Class Add_edit
         ' Lbl_LastName
         ' 
         Lbl_LastName.AutoSize = True
-        Lbl_LastName.Location = New Point(36, 30)
+        Lbl_LastName.Location = New Point(45, 38)
         Lbl_LastName.Margin = New Padding(2, 0, 2, 0)
         Lbl_LastName.Name = "Lbl_LastName"
-        Lbl_LastName.Size = New Size(82, 20)
+        Lbl_LastName.Size = New Size(99, 25)
         Lbl_LastName.TabIndex = 4
         Lbl_LastName.Text = "Last Name:"
         ' 
         ' Lbl_FirstName
         ' 
         Lbl_FirstName.AutoSize = True
-        Lbl_FirstName.Location = New Point(34, 68)
+        Lbl_FirstName.Location = New Point(42, 85)
         Lbl_FirstName.Margin = New Padding(2, 0, 2, 0)
         Lbl_FirstName.Name = "Lbl_FirstName"
-        Lbl_FirstName.Size = New Size(83, 20)
+        Lbl_FirstName.Size = New Size(101, 25)
         Lbl_FirstName.TabIndex = 5
         Lbl_FirstName.Text = "First Name:"
         ' 
         ' Lbl_Suffix
         ' 
         Lbl_Suffix.AutoSize = True
-        Lbl_Suffix.Location = New Point(67, 172)
+        Lbl_Suffix.Location = New Point(84, 215)
         Lbl_Suffix.Margin = New Padding(2, 0, 2, 0)
         Lbl_Suffix.Name = "Lbl_Suffix"
-        Lbl_Suffix.Size = New Size(49, 20)
+        Lbl_Suffix.Size = New Size(60, 25)
         Lbl_Suffix.TabIndex = 6
         Lbl_Suffix.Text = "Suffix:"
         ' 
         ' Lbl_MiddleName
         ' 
         Lbl_MiddleName.AutoSize = True
-        Lbl_MiddleName.Location = New Point(17, 106)
+        Lbl_MiddleName.Location = New Point(21, 132)
         Lbl_MiddleName.Margin = New Padding(2, 0, 2, 0)
         Lbl_MiddleName.Name = "Lbl_MiddleName"
-        Lbl_MiddleName.Size = New Size(103, 20)
+        Lbl_MiddleName.Size = New Size(123, 25)
         Lbl_MiddleName.TabIndex = 8
         Lbl_MiddleName.Text = "Middle Name:"
         ' 
         ' ComboBox_Suffix
         ' 
         ComboBox_Suffix.FormattingEnabled = True
-        ComboBox_Suffix.Location = New Point(117, 170)
-        ComboBox_Suffix.Margin = New Padding(2, 2, 2, 2)
+        ComboBox_Suffix.Location = New Point(146, 212)
+        ComboBox_Suffix.Margin = New Padding(2)
         ComboBox_Suffix.Name = "ComboBox_Suffix"
-        ComboBox_Suffix.Size = New Size(146, 28)
+        ComboBox_Suffix.Size = New Size(182, 33)
         ComboBox_Suffix.TabIndex = 9
         ' 
         ' DateTimePicker
         ' 
-        DateTimePicker.Location = New Point(117, 210)
-        DateTimePicker.Margin = New Padding(2, 2, 2, 2)
+        DateTimePicker.Location = New Point(146, 262)
+        DateTimePicker.Margin = New Padding(2)
         DateTimePicker.Name = "DateTimePicker"
-        DateTimePicker.Size = New Size(241, 27)
+        DateTimePicker.Size = New Size(300, 31)
         DateTimePicker.TabIndex = 10
         ' 
         ' Lbl_Birthdate
         ' 
         Lbl_Birthdate.AutoSize = True
-        Lbl_Birthdate.Location = New Point(42, 214)
+        Lbl_Birthdate.Location = New Point(52, 268)
         Lbl_Birthdate.Margin = New Padding(2, 0, 2, 0)
         Lbl_Birthdate.Name = "Lbl_Birthdate"
-        Lbl_Birthdate.Size = New Size(73, 20)
+        Lbl_Birthdate.Size = New Size(87, 25)
         Lbl_Birthdate.TabIndex = 11
         Lbl_Birthdate.Text = "Birthdate:"
         ' 
         ' ComboBox_Gender
         ' 
         ComboBox_Gender.FormattingEnabled = True
-        ComboBox_Gender.Location = New Point(117, 250)
-        ComboBox_Gender.Margin = New Padding(2, 2, 2, 2)
+        ComboBox_Gender.Location = New Point(146, 312)
+        ComboBox_Gender.Margin = New Padding(2)
         ComboBox_Gender.Name = "ComboBox_Gender"
-        ComboBox_Gender.Size = New Size(146, 28)
+        ComboBox_Gender.Size = New Size(182, 33)
         ComboBox_Gender.TabIndex = 12
         ' 
         ' Lbl_Gender
         ' 
         Lbl_Gender.AutoSize = True
-        Lbl_Gender.Location = New Point(54, 253)
+        Lbl_Gender.Location = New Point(68, 316)
         Lbl_Gender.Margin = New Padding(2, 0, 2, 0)
         Lbl_Gender.Name = "Lbl_Gender"
-        Lbl_Gender.Size = New Size(60, 20)
+        Lbl_Gender.Size = New Size(73, 25)
         Lbl_Gender.TabIndex = 13
         Lbl_Gender.Text = "Gender:"
         ' 
         ' ComboBox_CivilStatus
         ' 
         ComboBox_CivilStatus.FormattingEnabled = True
-        ComboBox_CivilStatus.Location = New Point(117, 290)
-        ComboBox_CivilStatus.Margin = New Padding(2, 2, 2, 2)
+        ComboBox_CivilStatus.Location = New Point(146, 362)
+        ComboBox_CivilStatus.Margin = New Padding(2)
         ComboBox_CivilStatus.Name = "ComboBox_CivilStatus"
-        ComboBox_CivilStatus.Size = New Size(146, 28)
+        ComboBox_CivilStatus.Size = New Size(182, 33)
         ComboBox_CivilStatus.TabIndex = 15
         ' 
         ' Lbl_CivilStatus
         ' 
         Lbl_CivilStatus.AutoSize = True
-        Lbl_CivilStatus.Location = New Point(31, 293)
+        Lbl_CivilStatus.Location = New Point(39, 366)
         Lbl_CivilStatus.Margin = New Padding(2, 0, 2, 0)
         Lbl_CivilStatus.Name = "Lbl_CivilStatus"
-        Lbl_CivilStatus.Size = New Size(84, 20)
+        Lbl_CivilStatus.Size = New Size(101, 25)
         Lbl_CivilStatus.TabIndex = 16
         Lbl_CivilStatus.Text = "Civil Status:"
         ' 
         ' Txt_InputNationality
         ' 
-        Txt_InputNationality.Location = New Point(117, 332)
-        Txt_InputNationality.Margin = New Padding(2, 2, 2, 2)
+        Txt_InputNationality.Location = New Point(146, 415)
+        Txt_InputNationality.Margin = New Padding(2)
         Txt_InputNationality.Name = "Txt_InputNationality"
-        Txt_InputNationality.Size = New Size(255, 27)
+        Txt_InputNationality.Size = New Size(318, 31)
         Txt_InputNationality.TabIndex = 17
         ' 
         ' Lbl_Nationality
         ' 
         Lbl_Nationality.AutoSize = True
-        Lbl_Nationality.Location = New Point(31, 334)
+        Lbl_Nationality.Location = New Point(39, 418)
         Lbl_Nationality.Margin = New Padding(2, 0, 2, 0)
         Lbl_Nationality.Name = "Lbl_Nationality"
-        Lbl_Nationality.Size = New Size(85, 20)
+        Lbl_Nationality.Size = New Size(101, 25)
         Lbl_Nationality.TabIndex = 18
         Lbl_Nationality.Text = "Nationality:"
         ' 
         ' Txt_InputContact
         ' 
-        Txt_InputContact.Location = New Point(117, 375)
-        Txt_InputContact.Margin = New Padding(2, 2, 2, 2)
+        Txt_InputContact.Location = New Point(146, 469)
+        Txt_InputContact.Margin = New Padding(2)
         Txt_InputContact.Name = "Txt_InputContact"
-        Txt_InputContact.Size = New Size(255, 27)
+        Txt_InputContact.Size = New Size(318, 31)
         Txt_InputContact.TabIndex = 19
         ' 
         ' Lbl_Contact
         ' 
         Lbl_Contact.AutoSize = True
-        Lbl_Contact.Location = New Point(24, 378)
+        Lbl_Contact.Location = New Point(30, 472)
         Lbl_Contact.Margin = New Padding(2, 0, 2, 0)
         Lbl_Contact.Name = "Lbl_Contact"
-        Lbl_Contact.Size = New Size(90, 20)
+        Lbl_Contact.Size = New Size(110, 25)
         Lbl_Contact.TabIndex = 20
         Lbl_Contact.Text = "Contact No.:"
         ' 
         ' Lbl_Address
         ' 
         Lbl_Address.AutoSize = True
-        Lbl_Address.Location = New Point(56, 421)
+        Lbl_Address.Location = New Point(70, 526)
         Lbl_Address.Margin = New Padding(2, 0, 2, 0)
         Lbl_Address.Name = "Lbl_Address"
-        Lbl_Address.Size = New Size(56, 20)
+        Lbl_Address.Size = New Size(70, 25)
         Lbl_Address.TabIndex = 21
         Lbl_Address.Text = "Adress:"
         ' 
         ' Txt_InputAddress
         ' 
-        Txt_InputAddress.Location = New Point(117, 418)
-        Txt_InputAddress.Margin = New Padding(2, 2, 2, 2)
+        Txt_InputAddress.Location = New Point(146, 522)
+        Txt_InputAddress.Margin = New Padding(2)
         Txt_InputAddress.Name = "Txt_InputAddress"
-        Txt_InputAddress.Size = New Size(255, 27)
+        Txt_InputAddress.Size = New Size(318, 31)
         Txt_InputAddress.TabIndex = 22
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(52, 602)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 23
+        Button1.Text = "Cancel"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(193, 602)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(112, 34)
+        Button2.TabIndex = 24
+        Button2.Text = "Clear"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(334, 602)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(112, 34)
+        Button3.TabIndex = 25
+        Button3.Text = "Save"
+        Button3.UseVisualStyleBackColor = True
         ' 
         ' Add_edit
         ' 
-        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(381, 498)
+        ClientSize = New Size(476, 648)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
         Controls.Add(Txt_InputAddress)
         Controls.Add(Lbl_Address)
         Controls.Add(Lbl_Contact)
@@ -265,7 +298,7 @@ Partial Class Add_edit
         Controls.Add(Txt_InputMiddleName)
         Controls.Add(Txt_InputFirstName)
         Controls.Add(Txt_InputLastName)
-        Margin = New Padding(2, 2, 2, 2)
+        Margin = New Padding(2)
         Name = "Add_edit"
         Text = "Regitration"
         ResumeLayout(False)
@@ -297,4 +330,8 @@ Partial Class Add_edit
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
 
     End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
