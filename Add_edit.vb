274cb5762f2bfd7f8faf0250aa28b9197e1f10be
lol <125685532+Ren-Desuwa@users.noreply.github.com>
2025-09-18 02:07:29 +0800

    database integration

--- Add_edit.vb
+++ Add_edit.vb
@@ -2,19 +2,13 @@
 Imports System.IO
 
 Public Class Add_edit
-    Dim con As OleDbConnection
-    Private Sub Add_edit_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim dbPath As String = Path.Combine(Application.StartupPath, "..\..\..\data\registration.accdb")
 
-        con = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & dbPath & ";")
-        Try
-            con.Open()
-            MessageBox.Show("Connected to Access!")
-        Catch ex As Exception
-            MessageBox.Show("Error: " & ex.Message)
-        Finally
-            con.Close()
-        End Try
+
+    Private Sub Add_edit_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
     End Sub
 
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
 End Class
