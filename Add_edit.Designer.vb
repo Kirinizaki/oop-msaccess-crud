64f5c65d69766b07e77adf8dca4bcacbc7b95176
Kirinizaki <jamesalba718@gmail.com>
2025-09-18 00:22:15 +0800

    this is it

--- Add_edit.Designer.vb
+++ Add_edit.Designer.vb
@@ -33,6 +33,16 @@ Partial Class Add_edit
         ComboBox1 = New ComboBox()
         DateTimePicker1 = New DateTimePicker()
         Label4 = New Label()
+        ComboBox2 = New ComboBox()
+        Label6 = New Label()
+        ComboBox4 = New ComboBox()
+        Label7 = New Label()
+        TextBox4 = New TextBox()
+        Label8 = New Label()
+        TextBox5 = New TextBox()
+        Label9 = New Label()
+        Label10 = New Label()
+        TextBox6 = New TextBox()
         SuspendLayout()
         ' 
         ' TextBox1
@@ -126,11 +136,103 @@ Partial Class Add_edit
         Label4.TabIndex = 11
         Label4.Text = "Birthdate:"
         ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Location = New Point(146, 313)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(182, 33)
+        ComboBox2.TabIndex = 12
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(67, 316)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(73, 25)
+        Label6.TabIndex = 13
+        Label6.Text = "Gender:"
+        ' 
+        ' ComboBox4
+        ' 
+        ComboBox4.FormattingEnabled = True
+        ComboBox4.Location = New Point(146, 363)
+        ComboBox4.Name = "ComboBox4"
+        ComboBox4.Size = New Size(182, 33)
+        ComboBox4.TabIndex = 15
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(39, 366)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(101, 25)
+        Label7.TabIndex = 16
+        Label7.Text = "Civil Status:"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(146, 415)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(318, 31)
+        TextBox4.TabIndex = 17
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(39, 418)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(101, 25)
+        Label8.TabIndex = 18
+        Label8.Text = "Nationality:"
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(146, 469)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(318, 31)
+        TextBox5.TabIndex = 19
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(30, 472)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(110, 25)
+        Label9.TabIndex = 20
+        Label9.Text = "Contact No.:"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(70, 526)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(70, 25)
+        Label10.TabIndex = 21
+        Label10.Text = "Adress:"
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(146, 523)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(318, 31)
+        TextBox6.TabIndex = 22
+        ' 
         ' Add_edit
         ' 
         AutoScaleDimensions = New SizeF(10.0F, 25.0F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(476, 560)
+        ClientSize = New Size(476, 622)
+        Controls.Add(TextBox6)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(TextBox5)
+        Controls.Add(Label8)
+        Controls.Add(TextBox4)
+        Controls.Add(Label7)
+        Controls.Add(ComboBox4)
+        Controls.Add(Label6)
+        Controls.Add(ComboBox2)
         Controls.Add(Label4)
         Controls.Add(DateTimePicker1)
         Controls.Add(ComboBox1)
@@ -159,6 +261,15 @@ Partial Class Add_edit
     Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents DateTimePicker1 As DateTimePicker
     Friend WithEvents Label4 As Label
-
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextBox6 As TextBox
 
 End Class
