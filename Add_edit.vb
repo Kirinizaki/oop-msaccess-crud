260e8d23219d99349b112c37286cee0ce44877b1
lol <125685532+Ren-Desuwa@users.noreply.github.com>
2025-09-18 00:44:28 +0800

    should run

--- Add_edit.vb
+++ Add_edit.vb
@@ -1,5 +1,16 @@
-﻿Public Class Add_edit
-    Private Sub Add_edit_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports System.Data.OleDb
 
+Public Class Add_edit
+    'Dim con As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\path\yourdb.accdb;")
+
+    Private Sub Add_edit_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Try
+        '    con.Open()
+        '    MessageBox.Show("Connected to Access!")
+        'Catch ex As Exception
+        '    MessageBox.Show("Error: " & ex.Message)
+        'Finally
+        '    con.Close()
+        'End Try
     End Sub
 End Class
